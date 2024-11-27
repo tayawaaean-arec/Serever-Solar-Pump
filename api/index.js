ba6f29d4ba1a9675239462bb9b969caa06461158
tayawaaean <arecaeangabrielle@gmail.com>
2024-11-27 09:52:25 +0800

    try

--- api/index.js
+++ api/index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const path = require('path');
 require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 // Initialize Firebase Admin SDK
-const serviceAccountPath = path.resolve(__dirname, process.env.GOOGLE_SERVICES_JSON || '../config/google-services.json');
+const serviceAccount = require(process.env.GOOGLE_SERVICES_JSON);
+
 admin.initializeApp({
-  credential: admin.credential.cert(require(serviceAccountPath)),
-  databaseURL: process.env.DATABASE_URL || 'https://solar-pump-98502-default-rtdb.firebaseio.com/',
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: firebaseConfig.databaseURL,
 });
 
 const database = admin.database();
